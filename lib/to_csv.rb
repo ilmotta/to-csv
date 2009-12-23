6506d5a6582915d5dddbaf686dd61542f0cf100c
Icaro Motta <icaro.ldm@gmail.com>
2009-12-23 17:56:36 -0200

    Small documentation improvements

--- lib/to_csv.rb
+++ lib/to_csv.rb
@@ -3,14 +3,14 @@ require 'ostruct'
 require 'active_support'
 require 'to_csv/csv_converter'
 
-module ToCSV  
+module ToCSV
   mattr_accessor :byte_order_marker, :locale, :primary_key, :timestamps
   mattr_accessor :csv_options
   self.csv_options = { :col_sep => ';' }
 end
 
 class Array
-  
+
   #
   # Returns a CSV string.
   #
@@ -22,23 +22,22 @@ class Array
   #      the beginning of the output. It's useful if you want to force
   #      MS Excel to read UTF-8 encoded files, otherwise it will just
   #      decode them as Latin1 (ISO-8859-1). Default: +false+.
-  #    +timestamps+::
-  #      Include timestamps (+created_at+ and +updated_at+) as columns.
-  #      Default: +false+.
   #    +only+::
   #      Same behavior as with the +to_json+ method.
   #    +except+::
   #      Same as +only+ option.
   #    +methods+::
   #      Accepts a symbol or an array with additional methods to be included.
+  #    +timestamps+::
+  #      Include timestamps +created_at+, +created_on+, +updated_at+ and
+  #      +updated_on+. If false Default: +false+.
   #    +primary_key+::
-  #      Has precedence over any other option. If +true+ the object's primary
-  #      key will be added as an attribute, which in turn will be mapped to
-  #      a CSV column. Default: +false+.
+  #      If +true+ the object's primary key will be added as an attribute,
+  #      which in turn will be mapped to a CSV column. Default: +false+.
   #    +headers+::
   #       If this list is <tt>nil</tt> then headers will be in alphabetical order.
   #       If it is an empty array or <tt>false</tt>, no headers will be shown.
-  #       If it is non empty, headers will sorted in the order specified.
+  #       If it is non empty, headers will be sorted in the order specified.
   #       <tt>:all</tt> can be used as a placeholder for all attributes not
   #       explicitly named.
   #    +locale+::
@@ -57,3 +56,4 @@ class Array
     csv_converter.to_csv
   end
 end
+
